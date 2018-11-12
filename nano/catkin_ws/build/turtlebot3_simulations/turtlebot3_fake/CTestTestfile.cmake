bb6415fb8f0b432f2cdb16fd9ff591c761bde814
Nano1201 <neverforever1201@gmail.com>
2018-11-12 15:29:06 +0000

    good

--- nano/catkin_ws/build/turtlebot3_simulations/turtlebot3_fake/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/test/InfoExchange/nano/catkin_ws/src/turtlebot3_simulations/turtlebot3_fake
-# Build directory: /home/ros/test/InfoExchange/nano/catkin_ws/build/turtlebot3_simulations/turtlebot3_fake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
