b6895a7790db5385cdc3d92844b105596f2baec2
Nano1201 <neverforever1201@gmail.com>
2019-01-03 04:22:48 +0000

    zed model (urdf format)

--- nano/zed/catkin_ws/build/catkin_generated/setup_cached.sh
+++ nano/zed/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,7 @@
 # new environment variables
 
 # modified environment variables
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/nvidia/lib:/usr/local/nvidia/lib64"
+export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/ros/test/InfoExchange/nano/zed/catkin_ws/build"
+export PYTHONPATH="/opt/ros/kinetic/lib/python2.7/dist-packages"
