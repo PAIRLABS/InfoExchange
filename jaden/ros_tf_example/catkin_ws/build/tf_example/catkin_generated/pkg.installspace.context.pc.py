45b0a831793606e0f2155860cd4761cc3ef89f22
jaden <jadenlin0106@nctu.edu.tw>
2019-01-22 16:57:53 +0800

    cleaned build folder and devel folder

--- jaden/ros_tf_example/catkin_ws/build/tf_example/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/ros/catkin_ws/install/include".split(';') if "/home/ros/catkin_ws/install/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;rospy;tf;turtlesim".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-ltf_example".split(';') if "-ltf_example" != "" else []
-PROJECT_NAME = "tf_example"
-PROJECT_SPACE_DIR = "/home/ros/catkin_ws/install"
-PROJECT_VERSION = "0.0.0"
