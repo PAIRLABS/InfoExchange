45d0c4bb1740adaee6b275841598e8679303c2d4
jaden <jadenlin0106@nctu.edu.tw>
2019-02-15 10:24:33 +0800

    adjust Alice and Andy's code, add secondary wheels at body.

---
+++ jaden/jaden-pairbot/src/pairbot/pb_control/scripts/right_sync.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float64
+
+def divide(a,b):
+    return Float64(a.data/b.data)
+
+pubs = []
+
+# right flipper is a mirror of right flipper, so angles of flippers are inversed (i,-i)
+def callback(data):
+    #coef=Float64(0.7)
+    #res=divide(data,coef)
+    #    rospy.loginfo(data)
+    rospy.logdebug("callback for right wheel")
+    rospy.logdebug("%s received!", str(data))
+    for p in pubs:
+        p.publish(data)
+
+T_RIGHT_BASE_WHEELS = "joint_right_base_wheel_{0}_controller/command"
+T_RIGHT_BASE_WHEELS_2nd = "joint_right_base_wheel_2nd_{0}_controller/command"
+T_RF_FW  = 'joint_right_front_flipper_wheel_controller/command'
+T_RR_FW  = 'joint_right_rear_flipper_wheel_controller/command'
+T_RF_FMW = 'joint_right_front_flipper_middle_wheel_{0}_controller/command'
+T_RR_FMW = 'joint_right_rear_flipper_middle_wheel_{0}_controller/command'
+T_RF_FMW_2nd = 'joint_right_front_flipper_middle_wheel_2nd_{0}_controller/command'
+T_RR_FMW_2nd = 'joint_right_rear_flipper_middle_wheel_2nd_{0}_controller/command'
+
+def listener():
+    rospy.init_node('listener', anonymous=True, log_level=rospy.DEBUG)
+    pubs.extend([ rospy.Publisher(T_RIGHT_BASE_WHEELS.format(i+1), Float64, queue_size=10) for i in range(12)])
+    pubs.extend([ rospy.Publisher(T_RIGHT_BASE_WHEELS_2nd.format(i+1), Float64, queue_size=10) for i in range(11)])
+    rospy.logdebug("There are %s publishers for right base wheels", str(len(pubs)))
+    base_wheel_number = len(pubs)
+
+    pubs.append(rospy.Publisher(T_RF_FW, Float64, queue_size=10))
+    pubs.append(rospy.Publisher(T_RR_FW, Float64, queue_size=10))
+    pubs.extend([ rospy.Publisher(T_RF_FMW.format(i+1), Float64, queue_size=10) for i in range(6)])
+    pubs.extend([ rospy.Publisher(T_RR_FMW.format(i+1), Float64, queue_size=10) for i in range(6)])
+    pubs.extend([ rospy.Publisher(T_RF_FMW_2nd.format(i+1), Float64, queue_size=10) for i in range(6)])
+    pubs.extend([ rospy.Publisher(T_RR_FMW_2nd.format(i+1), Float64, queue_size=10) for i in range(6)])
+    rospy.logdebug("There are %s publishers for right flipper wheels", str(len(pubs)-base_wheel_number))
+
+    rospy.Subscriber("right/command", Float64, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
